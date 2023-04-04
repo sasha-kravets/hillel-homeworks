0eec03c0666444c62cc18da35569110c695f6d7d
sasha-kravets <skravets.home@gmail.com>
2023-04-04 15:28:05 +0300

    homework-04042023

--- homework-12/index.php
+++ homework-12/index.php
@@ -63,6 +63,7 @@ print_r($numbers);
 echo '</pre>';
 
 // 6. ==================================================================
+
 $words = ['Japan', 'Canada', 'USA', 'Ukraine', 'Spain'];
 function lessThanNumber(array &$words, int $maxLength) : array
 {
