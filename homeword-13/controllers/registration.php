b6a5a1ab6adca9d0fe71d13f69f24c1825fe2c08
sasha-kravets <skravets.home@gmail.com>
2023-04-10 15:04:14 +0300

    Code Refactoring 10042023

--- homeword-13/controllers/registration.php
+++ homeword-13/controllers/registration.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../functions/functions.php';
 
 //1. Перевірити метод HTTP
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-  setMessages('Metgod is not allowed', 'warnings');
+  setMessages('Method is not allowed', 'warnings');
   header('Location: ../index.php');
 }
